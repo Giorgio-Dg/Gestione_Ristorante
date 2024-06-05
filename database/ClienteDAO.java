6836d4597390de2f07f58ad9168eae7db6f308a6
Giorgio-Dg <degregoriogiorgio3@gmail.com>
2024-06-05 23:08:03 +0200

    edit

--- database/ClienteDAO.java
+++ database/ClienteDAO.java
@@ -1,8 +1,62 @@
 package database;
 
-public class ClienteDAO {
+import java.util.ArrayList;
+
+public class ClienteDAO{
+	
 	private int IDCliente;
 	private String Nome;
 	private String Cognome;
 	private String Indirizzo;
+	private ArrayList<OrdineDAO> Ordini;
+	
+	public ClienteDAO(int iDCliente, String nome, String cognome, String indirizzo) {
+		IDCliente = iDCliente;
+		Nome = nome;
+		Cognome = cognome;
+		Indirizzo = indirizzo;
+		Ordini = new ArrayList<OrdineDAO>();
+	}
+
+	public int getIDCliente() {
+		return IDCliente;
+	}
+
+	public void setIDCliente(int iDCliente) {
+		IDCliente = iDCliente;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCognome() {
+		return Cognome;
+	}
+
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+
+	public String getIndirizzo() {
+		return Indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		Indirizzo = indirizzo;
+	}
+
+	public ArrayList<OrdineDAO> getOrdini() {
+		return Ordini;
+	}
+
+	public void setOrdini(ArrayList<OrdineDAO> ordini) {
+		Ordini = ordini;
+	}
+	
 }
+
--- entity/EntityMenu.java
+++ entity/EntityMenu.java
@@ -1,5 +1,20 @@
 package entity;
 
-public class EntityMenu {
+import java.util.ArrayList;
 
+public class EntityMenu {
+	private ArrayList<EntityPiatto> Piatti;
+
+	public EntityMenu(ArrayList<EntityPiatto> piatti) {
+		Piatti = piatti;
+	}
+
+	public ArrayList<EntityPiatto> getPiatti() {
+		return Piatti;
+	}
+
+	public void setPiatti(ArrayList<EntityPiatto> piatti) {
+		Piatti = piatti;
+	}
+	
 }
