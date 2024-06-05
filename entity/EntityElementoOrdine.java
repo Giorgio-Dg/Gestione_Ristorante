dbeb533d52ea15a3bde27852025f91efb6bb5f05
Giorgio-Dg <degregoriogiorgio3@gmail.com>
2024-06-05 22:59:16 +0200

    edit by GDG

--- entity/EntityElementoOrdine.java
+++ entity/EntityElementoOrdine.java
@@ -1,1 +1,40 @@
+package entity;
+
+public class EntityElementoOrdine {
+	
+	private EntityOrdine Ordine;
+	private EntityPiatto Piatto;
+	private int Quantita;
+	
+	public EntityElementoOrdine(EntityOrdine ordine, EntityPiatto piatto, int quantita) {
+		Ordine = ordine;
+		Piatto = piatto;
+		Quantita = quantita;
+	}
+
+	public EntityOrdine getOrdine() {
+		return Ordine;
+	}
+
+	public void setOrdine(EntityOrdine ordine) {
+		Ordine = ordine;
+	}
+
+	public EntityPiatto getPiatto() {
+		return Piatto;
+	}
+
+	public void setPiatto(EntityPiatto piatto) {
+		Piatto = piatto;
+	}
+
+	public int getQuantita() {
+		return Quantita;
+	}
+
+	public void setQuantita(int quantita) {
+		Quantita = quantita;
+	}
+	
+}
 
