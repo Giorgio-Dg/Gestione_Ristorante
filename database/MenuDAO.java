2b6ab4a8b28b618d0ae8470662627d55bf44a4a4
Giorgio-Dg <degregoriogiorgio3@gmail.com>
2024-06-06 12:20:05 +0200

    edit by gdg

--- database/MenuDAO.java
+++ database/MenuDAO.java
@@ -1,10 +1,30 @@
 package database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MenuDAO {
 	private ArrayList<PiattoDAO> Piatti;
 
+	public MenuDAO() {
+		super();
+	}
+
+	public void caricaDaDB() {
+		
+		String query = new String("select * from piatto where Nome ='"+this.Nome+"';");
+		
+		try {
+			ResultSet rs = DBConnectionManager.selectQuery(query);
+			
+			if(rs.next()) this.setDescrizione(rs.getString("Descrizione"));
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public MenuDAO(ArrayList<PiattoDAO> piatti) {
 		Piatti = piatti;
 	}
