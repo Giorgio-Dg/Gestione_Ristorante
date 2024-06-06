c82a683347ac6994703954bf541226734225c68d
Giorgio-Dg <degregoriogiorgio3@gmail.com>
2024-06-06 11:26:12 +0200

    edit by gdg

--- database/PiattoDAO.java
+++ database/PiattoDAO.java
@@ -1,13 +1,37 @@
 package database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class PiattoDAO {
 	private String Nome;
 	private String Descrizione;
 	
-	public PiattoDAO(String nome, String descrizione) {
-		Nome = nome;
-		Descrizione = descrizione;
+	public PiattoDAO() {
+		super();
 	}
+	
+	//costruttore che prende in ingresso la PK
+	public PiattoDAO(String nome) {
+		this.Nome = nome;
+		caricaDaDB();
+	}
+
+	public void caricaDaDB() {
+		
+		String query = new String("select * from piatto where Nome ='"+this.Nome+"';");
+		
+		try {
+			ResultSet rs = DBConnectionManager.selectQuery(query);
+			
+			if(rs.next()) this.setDescrizione(rs.getString("Descrizione"));
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public String getNome() {
 		return Nome;
 	}
