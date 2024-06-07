9a98d4473ae527d5f56d7305205cfe7dcd26a51c
Packy777 <pasqualegraziosi10@gmail.com>
2024-06-07 18:11:48 +0200

    mmmm

--- boundary/MainFrame.java
+++ boundary/MainFrame.java
@@ -40,3 +40,5 @@ public class MainFrame extends JFrame {
 	}
 
 }
+
+lppp
--- entity/EntityElencoClienti.java
+++ entity/EntityElencoClienti.java
@@ -1,5 +1,23 @@
 package entity;
 
+import java.util.ArrayList;
+import database.ClienteDAO;
+
 public class EntityElencoClienti {
 
+	public EntityElencoClienti() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ArrayList<ClienteDAO> elencoClienti(){
+		
+		
+		ArrayList<ClienteDAO> clienti = new ArrayList<ClienteDAO>();		
+						
+		//clienti = ClienteDAO.caricaTuttiClientidaDB();
+		
+				
+		return clienti;
+	}
+	
 }
