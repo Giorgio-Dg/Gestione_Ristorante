25ba4175bbf79b8b9fa7c8ebc04998c0fffbf023
Giorgio-Dg <degregoriogiorgio3@gmail.com>
2024-06-14 14:52:21 +0200

    edit

--- boundary/MainFrame.java
+++ boundary/MainFrame.java
@@ -1,13 +1,11 @@
 package boundary;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
